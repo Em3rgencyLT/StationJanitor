2415f129abc513ad158e853563e01dac90e7e0f7
Em3rgencyLT <em3rgency.lt@gmail.com>
2018-01-14 13:49:51 +0200

    Add project files.

---
+++ StationJanitor/Controllers/ExampleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandAndConquer.CLI.Attributes;
+
+namespace StationJanitor.Controllers
+{
+	[CliController("example", "This is a description of the controller.")]
+	public static class ExampleController
+	{
+		[CliCommand("test", "This is an example of how you can setup methods.")]
+		public static void MethodExample(string something, List<string> someMoreThings = null)
+		{
+			Console.WriteLine(something);
+			foreach (var thing in someMoreThings)
+			{
+				Console.WriteLine(thing);
+			}
+		}
+	}
+}
---
+++ StationJanitor/Program.cs
@@ -0,0 +1,12 @@
+﻿using CommandAndConquer.CLI.Core;
+
+namespace StationJanitor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Processor.ProcessArguments(args);
+		}
+	}
+}
